754fe0ae451595983bb4fd2cd05f630c3de71711
Ben You <byou729@gmail.com>
2018-07-14 13:15:34 -0500

    screenshoot added

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
-var keys = require('keys.js')
-var spotify = new Spotify(keys.spotify);
+// var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var keys = require('./keys.js')
+// var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var request = require("request");
 var fs = require("fs");
